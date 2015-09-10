446f6e4cb1269fe4ac2101566bd6285d5edbf556
Nick Colavita <nickcolavita88@gmail.com>
2015-09-10 17:06:11 -0400

    Fixed relationships between Login and Sign Up. Created a Profile Page for each user.

--- app.rb
+++ app.rb
@@ -5,15 +5,23 @@ require "rack-flash"
 require "./models"
 
 set :database, "sqlite3:profiles.sqlite3"
+set :sessions, true
+use Rack::Flash, sweep: true
+
+def current_user
+	if session[:user_id]
+		User.find(session[:user_id])
+	else
+		nil
+	end
+end
 
 get "/" do
 	erb :index 	
 end
 
 get "/login" do
-	erb :login 
-# if statement for not logging in right 
-# 	redirect to "/news"
+	erb :login
 end
 
 get "/signup" do
@@ -22,6 +30,7 @@ get "/signup" do
 end
 
 get "/profile" do
+	@user = current_user
 	erb :profile 
 end
 
@@ -44,5 +53,20 @@ get "/news" do
 end
 
 post "/signup" do
-	User.create(fname: params[:fname], lname: params[:lname], username: params[:username], email: params[:email], password: params[:password])
-end 	
+	# user = User.create(params[:user])
+	user = User.create(fname: params[:fname], lname: params[:lname], username: params[:username], email: params[:email], password: params[:password])
+	session[:user_id] = user.id
+	p user
+	redirect to '/profile'
+end 
+
+post "/sessions" do
+	user = User.find_by(email: params[:email])
+	if user and user.password == params["password"]
+		session[:user_id] = user.id
+		redirect to '/news'
+	else
+		flash[:notice] = "There was a problem logging in."
+		redirect to 'login'
+	end
+end	
