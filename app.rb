5c18ec6e6b35592300935507bd1090882e20a8e2
Nick Colavita <nickcolavita88@gmail.com>
2015-09-11 13:03:23 -0400

    Added functionality to the delete button in Users Profile. Added Functionality to the Users List page to link properly to each Users Profile Page.

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ set :database, "sqlite3:profiles.sqlite3"
 set :sessions, true
 use Rack::Flash, sweep: true
 
+#This defines the current user by their session id.
 def current_user
 	if session[:user_id]
 		User.find(session[:user_id])
@@ -34,26 +35,60 @@ get "/profile" do
 	erb :profile 
 end
 
+post "/profile" do
+	@user = current_user
+	current_user.destroy
+	redirect to '/'
+end
+
 get "/edit" do
 	@users = current_user
 	erb :edit 
 # 	redirect to "/profile"
 end
 
+#This is listing all of the users on the User_List page
 get "/list" do
 	@users = User.all
 	erb :user_list 
 end
 
-post "/list" do
-	@user = User.new(fname: params[:fname], lname: params[:lname])
+# This is getting the users ID and linking to the show page of the users Profile if the id is a match
+get '/users/:id' do 
+	begin 
+		@user = User.find(params[:id])
+		erb :show
+	rescue
+		flash[:notice] = "That User Does Not Exist"
+		redirect to '/'
+	end
 end
 
+#This is displaying all of the posts currently created by all users
 get "/news" do
 	@posts = Post.all
 	erb :news_feed
 end
 
+get "/logout" do
+	session[:user_id] = nil
+	flash[:notice] = "Logged out!"
+	redirect to '/login'
+end
+
+post '/' do
+	if session[:user_id] != nil
+		flash[:notice] = "Welcome User"
+	else
+		flash[:notice] = "Log in or Sign Up"
+	end
+end
+
+post "/list" do
+	@users = User.all
+end
+
+#This signs up a new user and stores their information into the database linking the form input to the columns predetermined in the database
 post "/signup" do
 	# user = User.create(params[:user])
 	user = User.create(fname: params[:fname], lname: params[:lname], username: params[:username], email: params[:email], password: params[:password])
@@ -69,7 +104,7 @@ post "/sessions" do
 		redirect to '/news'
 	else
 		flash[:notice] = "There was a problem logging in."
-		redirect to 'login'
+		redirect to '/login'
 	end
 end	
 
